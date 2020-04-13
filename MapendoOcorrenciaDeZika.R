d99cb8af1338485be8e2498ac126866738eaefca
joao souza <joaosouzaps9@gmail.com>
2020-04-13 12:33:50 -0300

    Obtendo as localidade unicas

--- MapendoOcorrenciaDeZika.R
+++ MapendoOcorrenciaDeZika.R
@@ -48,6 +48,72 @@ brazil %>% filter(location_type == "region") %>%
   geom_point() +
   ggtitle("Casos de Zika por Região do Brasil")
 
+# Separando as regiões e visualizando os dados
+region <- brazil %>% 
+  filter(location_type == "region")
+
+region %>% 
+  ggplot(aes(x =location, y = value)) + geom_bar(stat = "identity") +
+  ylab("Número de Casos Reportados") + xlab("Region") + 
+  ggtitle("Casos de Zika Reportados no Brasil")
+
+region %>% 
+  slice(1:length(unique(region$location))) %>% 
+  arrange(desc(value)) %>%
+  mutate(location = factor(location, levels = location,ordered = TRUE)) %>%
+  ggplot(aes(x = location, y = value)) + geom_bar(stat = "identity") +
+  ylab("Número de Casos Reportados") + xlab("Region") + 
+  ggtitle("Casos de Zika Reportados no Brasil")
+
+# Obtendo localidades únicas
+region %>% 
+  slice(1:length(unique(region$location)))
+
+# Organziando as localidades únicas por número de casos reportados
+region %>% 
+  slice(1:length(unique(region$location))) %>% 
+  arrange(desc(value))
+
+# Criando variáveis do tipo fator
+region %>% 
+  slice(1:length(unique(region$location))) %>% 
+  arrange(desc(value)) %>%
+  mutate(location = factor(location,levels=location,ordered=TRUE)) %>% 
+  glimpse()
+
+# Agrupando e sumarizando
+brazil_totals <- brazil %>% filter(location=="Brazil") 
+region_totals <- brazil %>% filter(location_type=="region") %>%
+  group_by(report_date,location) %>%  
+  summarize(tot = sum(value)) 
+
+# Padronizar os dados e remover as sumarizações
+regvec <- vector()  
+length(regvec) <- nrow(brazil)
+for (ii in 1:nrow(brazil)) {
+  if (brazil[ii,]$location_type != "region")  {
+    regvec[ii] <- newlab
+  } else {
+    newlab <- brazil[ii,]$location
+    regvec[ii] <- newlab
+  }
+}
+
+# Agregando o vetor de regiões ao dataframe brasil
+statedf <- cbind(brazil,regvec)
+
+# Eliminar o sumário de linhas por região e país
+statedf <- statedf %>% filter(location != "Brazil") 
+statedf <- statedf %>% filter(location_type != "region")
+
+# Gerar o total por regiões a partir dos dados transformados
+statedf %>% group_by(report_date,regvec) %>% 
+  summarize(tot=sum(value)) -> totals
+
+
+
+
+
 
 
 
